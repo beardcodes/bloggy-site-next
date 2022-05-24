273c30315e6ddcf16fe86c5bb91810732a903526
molessom-8 <elquaraoui@gmail.com>
2022-05-25 00:22:19 +0800

    fun blog

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+function Nav() {
+  return (
+    <nav>
+      <Link href="/">Bloggy Site</Link>
+    </nav>
+  );
+}
+
+export default Nav;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["media.graphassets.com", "localhost"],
+  },
 }
 
 module.exports = nextConfig
